a0b187a5184addbd356d3ba628c0cc3902f61eb0
mastlouis <mastlouis@wpi.edu>
2020-05-16 09:29:16 -0700

    Organized the  Code, made strings take fewer messages

--- index.js
+++ index.js
@@ -38,72 +38,16 @@ BOT.on('message', msg=>{
             msg.reply('Greetings, Human! \\*Beep Boop\\*');
             break;
         case 'THEMIND':
-            // msg.reply("FOCUS")
-            if(args[1] === 'version'){
-                msg.channel.send('Version 0.1.0');
-            }
-            else if(args[1] === 'usage'){
-                msg.channel.send('THEMIND [PLAYER COUNT] [ROUND NUMBER]');
-            }
-            else if(args.length < 3){
-                msg.reply('Remember to include a player count, then a round number.');
-            }
-            else{
-                let players = args[1];
-                let round = args[2];
-                let deck = [];
-                playerNames = NAMES;
-                // playerNames = []; for(i = 0; i < players; i++){ playerNames.push(`Player ${i + 1}`); }
-                for(i = 0; i < 1000000; i++){// Make an array with numbers 1-100
-                    deck.push(i + 1);
-                }
-                msg.channel.send(`Starting round ${round} with ${players} players. Cards range from ${deck[0]} to ${deck[deck.length - 1]}.`);
-                deck = shuffle(deck);
-                // msg.channel.send("Here's the unshuffled deck: " + deck);
-
-                playerCards = [];
-                for(i = 0; i < players; i++){
-                    hand = [];
-                    for(j = 0; j < round; j++){
-                        hand.push(deck.pop());
-                    }
-                    playerCards.push(hand);
-                }
-                for(i = 0; i < players; i++){
-                    let tail = String(Math.floor(Math.random() * 1000000));
-                    msg.channel.send(`${playerNames[i]}'s cards: ||Hand: ${playerCards[i].sort((a, b)=> a - b)}\\_\\_\\_\\_\\_Tail: ${tail}||`);
-                }
-            }
+            theMind(msg, args);
             break;
         case 'TEAMS':
-            if(args.length < 2){
-                msg.reply('Usage: TEAMS [NUMBER OF TEAMS]');
-                return;
-            }
-            teams = new Array(args[1]);
-            for(i = 0; i < args[1]; i++){
-                teams[i] = [];
-            }
-            playerNames = shuffle(NAMES);
-            // playerNames = []; for(i = 0; i < players; i++){ playerNames.push(`Player ${i + 1}`); }
-            while(playerNames.length > 0){
-                for(i = 0; i < teams.length; i++){
-                    if(playerNames.length > 0){
-                        teams[i].push(playerNames.pop());
-                    }
-                }
-            }
-            for(i = 0; i < args[1]; i++){
-                msg.channel.send(`Team ${i + 1}: ${teams[i]}`);
-            }
+            teams(msg, args);
             break;
         case 'COLORS':
-            msg.channel.send(`Colors: ${shuffle(['Red', 'Blue'])}`);
+            msg.channel.send(`Colors: ${ shuffle(['Red', 'Blue']) }`);
             break;
         case 'DRUMROLL':
-            for(i = 0; i < 6; i++){
-                msg.channel.send('\\*Drums\\*');
-            }
+            for(i = 0; i < 6; i++){ msg.channel.send('\\*Drums\\*'); }
             break;
         case 'FOCUS':
             msg.channel.send('https://media.giphy.com/media/l4FGHqrRZNPkfoLCw/giphy.gif');
@@ -125,3 +69,73 @@ BOT.on('message', msg=>{
     }
 });
 BOT.login(TOKEN);
+
+function theMind(msg, args){
+    if(args.length < 1){
+        msg.reply('Remember to include a player count, then a round number.');
+    }
+    if(args[1] === 'version'){
+        msg.channel.send('Version 0.1.0');
+    }
+    else if(args[1] === 'usage'){
+        msg.channel.send('THEMIND [PLAYER COUNT] [ROUND NUMBER]');
+    }
+    else if(args.length < 3){
+        msg.reply('Remember to include a player count, then a round number.');
+    }
+    else{
+        let players = args[1];
+        let round = args[2];
+        let max = 100;
+        let deck = [];
+        let playerNames = NAMES.slice();  // Make a copy
+        // let playerNames = []; for(i = 0; i < players; i++){ playerNames.push(`Player ${i + 1}`); }
+        let playerCards = [];
+        for(i = 0; i < max; i++){// Make an array with numbers 1-100
+            deck.push(i + 1);
+        }
+        let out =`Starting round ${round} with ${players} players. Cards range from ${deck[0]} to ${deck[deck.length - 1]}.`;
+        deck = shuffle(deck);
+
+        for(i = 0; i < players; i++){
+            let hand = [];
+            for(j = 0; j < round; j++){
+                hand.push(deck.pop());
+            }
+            playerCards.push(hand);
+        }
+        for(i = 0; i < players; i++){
+            let tail = String(Math.floor(Math.random() * 1000000));
+            if(i >= playerNames.length){
+                playerNames.push(`Player ${i + 1}`);
+            }
+            out += `\n${playerNames[i]}'s cards: ||Hand: ${playerCards[i].sort((a, b)=> a - b)}\\_\\_\\_\\_\\_Tail: ${tail}||`;
+        }
+        msg.channel.send(out);
+    }
+}
+
+function teams(msg, args){
+    if(args.length < 2){
+        msg.reply('Usage: TEAMS [NUMBER OF TEAMS]');
+        return;
+    }
+    teams = new Array(args[1]);
+    for(i = 0; i < args[1]; i++){
+        teams[i] = [];
+    }
+    playerNames = shuffle(NAMES);
+    // playerNames = []; for(i = 0; i < players; i++){ playerNames.push(`Player ${i + 1}`); }
+    while(playerNames.length > 0){
+        for(i = 0; i < teams.length; i++){
+            if(playerNames.length > 0){
+                teams[i].push(playerNames.pop());
+            }
+        }
+    }
+    out = "";
+    for(i = 0; i < args[1]; i++){
+        out += `\nTeam ${i + 1}: ${teams[i]}`;
+    }
+    msg.channel.send(out.substring(1, out.length));
+}
